1d36a48f4f85ae65b629bc6b5816a20795d0a895
ccadona77 <profissionalcadona@gmail.com>
2024-10-22 14:47:59 -0300

    Adicionei a biblioteca do DHT

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <Arduino.h>
 
-//  PINOS CONSTANTES
-const int a = 1;
+// CONSTANTS
+const float MAX_TEMP = 22.0;  // Valor a ser definido pelo botão externo
+const float MIN_TEMP = 18.0;  // Valor a ser definido pelo botão externo
+const int ENOUGH_LIGHT = 150; // Valor de luminosidade suficiente (Quanto menor o número, mais luminoso)
+const int DRY = 700;          // Valor lido quando o solo está totalmente seco
+const int WET = 370;          // Valor lido quando o solo está submerso na água
+
+//  Digital Pin's
+const int hotLightPin = 12;
+const int ledLightPin = 14;
+const int dhtPins = 27;
+const int fanPin = 26;
+const int valvePin = 25;
+
+//  Analog Pin's
+const int ldrPins[2] = {33, 32};
+const int capacitiveSensor = 35;
 
 //---------------------------------------------------------------------------------
 // Setup:

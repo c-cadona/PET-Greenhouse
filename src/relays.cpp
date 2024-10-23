dbcabcbef79568f3711fa2d7d587dbd96dab8fa1
ccadona77 <profissionalcadona@gmail.com>
2024-10-23 20:57:49 -0300

    Funções controladoras
    Fiz as funções controladoras dos periféricos nos relés.
    - FAN;
    - Luz quente;
    - Luz fria;
    - Válvula solenoide.

--- src/relays.cpp
+++ src/relays.cpp
@@ -9,3 +9,50 @@ void setupRelays(const int valve, const int fan, const int ledLight, const int h
     pinMode(ledLight, OUTPUT);
     pinMode(hotLight, OUTPUT);
 }
+
+//  Função responsável por ligar a FAN
+void fanON(const int fan)
+{
+    digitalWrite(fan, LOW);
+}
+
+//  Função responsável por desligar a FAN
+void fanOFF(const int fan)
+{
+    digitalWrite(fan, LOW);
+}
+
+//  Função responsável por ligar a lâmpada de secagem
+void hotLightON(const int hotLight)
+{
+    digitalWrite(hotLight, LOW);
+}
+
+//  Função responsável por desligar a lâmpada de secagem
+void hotLightOFF(const int hotLight)
+{
+    digitalWrite(hotLight, HIGH);
+}
+
+//  Função responsável por ligar a lâmpada led
+void ledLightON(const int ledLight)
+{
+    digitalWrite(ledLight, LOW);
+}
+
+//  Função responsável por desligar a lâmpada de secagem
+void ledLightOFF(const int ledLight)
+{
+    digitalWrite(ledLight, HIGH);
+}
+
+//  Função responsável por ligar a válvula solenoide
+void valveON(const int valve)
+{
+    digitalWrite(valve, LOW);
+}
+//  Função responsável por desligar a válvula solenoide
+void valveOFF(const int valve)
+{
+    digitalWrite(valve, HIGH);
+}
--- src/relays.h
+++ src/relays.h
@@ -3,4 +3,16 @@
 
 void setupRelays(const int valve, const int fan, const int ledLight, const int hotLight);
 
+void fanON(const int fan);
+void fanOFF(const int fan);
+
+void hotLightON(const int hotLight);
+void hotLightOFF(const int hotLight);
+
+void ledLightON(const int ledLight);
+void ledLightOFF(const int ledLight);
+
+void valveON(const int valve);
+void valveOFF(const int valve);
+
 #endif
